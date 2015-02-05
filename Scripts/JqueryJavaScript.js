08ccf45b1cd7fdfd0b27714ea90ede60b8c196aa
mitf11m048 <mitf11m048@pucit.edu.pk>
2015-02-06 01:59:43 +0500

    My Last Modification

---
+++ Scripts/JqueryJavaScript.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(
+    function () {
+
+        
+            $("#p1").hide(0);
+            $(".headingClass").hide(0);
+       /* 
+        $("#headimg").animate({
+            opacity: 0.25,
+            left: "+=50",
+            height: "toggle"
+        }, 5000, function () {
+            // Animation complete.
+            */
+
+
+            $("#p1").slideDown("slow");
+            $(".headingClass").slideDown("slow");
+        });
+       
+        
+    });
